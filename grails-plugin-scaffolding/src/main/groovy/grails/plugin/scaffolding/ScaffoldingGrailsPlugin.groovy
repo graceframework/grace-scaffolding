e093c804ad447fef855edb6ea41cde8c03cf46d5
Michael Yan <rain@rainboyan.com>
2022-05-28 21:35:23 +0800

    Update grailsVersion

--- grails-plugin-scaffolding/src/main/groovy/grails/plugin/scaffolding/ScaffoldingGrailsPlugin.groovy
+++ grails-plugin-scaffolding/src/main/groovy/grails/plugin/scaffolding/ScaffoldingGrailsPlugin.groovy
@@ -6,7 +6,7 @@ import org.grails.web.servlet.view.GroovyPageViewResolver
 class ScaffoldingGrailsPlugin extends Plugin {
 
    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "4.0.0.BUILD-SNAPSHOT > *"
+    def grailsVersion = "5.0.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
